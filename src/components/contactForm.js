3813739d3fef0932def27b87df7676d435346813
Fahim Chowdhury <fahimchowdhury950@gmail.com>
2022-05-06 15:42:29 +0600

    required field fixed

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -31,13 +31,13 @@ export default function ContactUs() {
                 <form onSubmit={sendEmail} className="signin-form">
                     <div className="row pt-5 mx-auto">
                     <div className="col-8 form-group pt-2 mx-auto">
-                            <input type="text" className="form-control" placeholder="Subject" name="subject"/>
+                            <input required type="text" className="form-control" placeholder="Subject" name="subject"/>
                         </div>
                         <div className="col-8 form-group mx-auto">
-                            <input type="text" className="form-control" placeholder="Name" name="name"/>
+                            <input required type="text" className="form-control" placeholder="Name" name="name"/>
                         </div>
                         <div className="col-8 form-group pt-2 mx-auto">
-                            <input type="email" className="form-control" placeholder="Email Address" name="email"/>
+                            <input required type="email" className="form-control" placeholder="Email Address" name="email"/>
                         </div>
                         <div className="col-8 form-group pt-2 mx-auto">
                             <input type="text" className="form-control" placeholder="Meet Link" name="meet"/>
@@ -46,7 +46,7 @@ export default function ContactUs() {
                             <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message"></textarea>
                         </div>
                         <div className="col-8 pt-3 mx-auto">
-                            <input type="submit" className="btn btn-info" value="Send Mail"></input>
+                            <input required type="submit" className="btn btn-info" value="Send Mail"></input>
                         </div>
                     </div>
                 </form>
